d0fa74b7159f2b5503d135c286734022e31f7d06
kazurayam <kazuaki.urayama@gmail.com>
2022-03-04 09:05:12 +0900

    use materialstore-0.4.6-SNAPSHOT.jar for MProductGroupBuilder

--- Scripts/missions/NISA/reduce_chronos/Script1646297043133.groovy
+++ Scripts/missions/NISA/reduce_chronos/Script1646297043133.groovy
@@ -1,54 +1,19 @@
-import com.kazurayam.materialstore.MaterialstoreFacade
-import com.kazurayam.materialstore.filesystem.FileType
-import com.kazurayam.materialstore.filesystem.JobName
-import com.kazurayam.materialstore.filesystem.JobTimestamp
-import com.kazurayam.materialstore.filesystem.Material
+import java.util.function.BiFunction
+
 import com.kazurayam.materialstore.filesystem.MaterialList
-import com.kazurayam.materialstore.filesystem.Store
-import com.kazurayam.materialstore.map.Mapper
-import com.kazurayam.materialstore.map.MappedResultSerializer
-import com.kazurayam.materialstore.mapper.ExcelToCsvMapperPOI3
-import com.kazurayam.materialstore.metadata.Metadata
-import com.kazurayam.materialstore.metadata.QueryOnMetadata
-import com.kazurayam.materialstore.reduce.MProduct
 import com.kazurayam.materialstore.reduce.MProductGroup
-import com.kms.katalon.core.util.KeywordUtil
-import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
-import groovy.json.JsonOutput
+import com.kazurayam.materialstore.reduce.MProductGroupBuilder
 
 assert store != null
 assert currentMaterialList != null
 
-JobName jobName = currentMaterialList.getJobName()
-JobTimestamp currentTimestamp = currentMaterialList.getJobTimestamp()
-JobTimestamp previousTimestamp =
-	store.queryJobTimestampWithSimilarContentPriorTo(jobName, currentTimestamp)
+BiFunction<MaterialList, MaterialList, MProductGroup> func = {
+    MaterialList left, MaterialList right ->
+	    MProductGroup.builder(left, right)
+		    .build()
+}
 
-WebUI.comment("reduce started; jobName=${jobName}, store=${store}")
-WebUI.comment("reduce started; previousTimestamp=${previousTimestamp}")
-WebUI.comment("reduce started; currentTimestamp=${currentTimestamp}")
+MProductGroup reduced = MProductGroupBuilder.chronos(store, currentMaterialList, func)
+assert reduced != null
 
-// Look up the materials stored in the previous time of run
-MaterialList left = store.select(jobName, previousTimestamp, QueryOnMetadata.ANY)
-assert left.size() > 0
-
-// Look up the materials stored in the current time of run
-MaterialList right = store.select(jobName, currentTimestamp, QueryOnMetadata.ANY)
-assert right.size() > 0
-
-// zip 2 Materilas to form a single Artifact
-MProductGroup prepared =
-	MProductGroup.builder(left, right)
-		.build()
-assert prepared.size() > 0
-
-//println JsonOutput.prettyPrint(prepared.toString())
-		
-// make diff with 2 Materials and record it in a single Artifact
-MaterialstoreFacade facade = MaterialstoreFacade.newInstance(store)
-
-MProductGroup reduced = facade.reduce(prepared)
-
-return reduced
-
-
+return reduced    
